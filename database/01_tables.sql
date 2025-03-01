f614d5d2df42cee8d64f2e6e17fcb9687f7d989c
Mengnali_Dou <1004718475@qq.com>
2025-03-01 19:08:25 +0800

    add constraint "fk_movie_hall_movie_hall_type" to table "tb_movie_hall_type" delete table "tb_movie_hall_type"'s columns "row_numbers" and "col_numbers"

--- database/01_tables.sql
+++ database/01_tables.sql
@@ -45,7 +45,8 @@ create table tb_movie_hall
     create_time        datetime      default CURRENT_TIMESTAMP                                                                                null comment '创建时间',
     update_time        datetime      default CURRENT_TIMESTAMP                                                                                null on update CURRENT_TIMESTAMP comment '更新时间',
     deleted            tinyint       default 0                                                                                                not null comment '是否删除 0 - 未删除 1 - 已删除',
-    constraint fk_movie_hall_cinema foreign key (cinema_id) references tb_cinema (cinema_id)
+    constraint fk_movie_hall_cinema foreign key (cinema_id) references tb_cinema (cinema_id),
+    constraint fk_movie_hall_movie_hall_type foreign key (movie_hall_type_id) references tb_movie_hall_type (type_id)
 ) comment '影厅';
 
 
@@ -53,8 +54,6 @@ create table tb_movie_hall_type
 (
     type_id     bigint auto_increment comment 'id' primary key,
     type_name   varchar(256)                       not null comment '类型名',
-    row_numbers int                                not null comment '座位排数',
-    col_numbers int                                not null comment '座位列数',
     create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
     update_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
     deleted     tinyint  default 0                 not null comment '是否删除 0 - 未删除 1 - 已删除'
