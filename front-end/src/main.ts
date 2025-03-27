0205bb502518b537cdf4c2f9475a0bacf8a8fff2
Mengnali_Dou <1004718475@qq.com>
2025-03-27 11:17:11 +0800

    install pinia & vue-router

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
+app.mount('#app');
