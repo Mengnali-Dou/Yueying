806995ddafb77622a41c17532fb4070a494295b4
Mengnali_Dou <1004718475@qq.com>
2025-03-27 14:48:04 +0800

    add i18n

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -3,11 +3,14 @@ import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { setupI18n } from "./plugins/i18n.ts";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 pinia.use(piniaPluginPersistedstate);
+
+setupI18n(app);
 
 app.use(pinia);
 app.mount("#app");
